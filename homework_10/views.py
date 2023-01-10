f3b839eac625e1015e3d9e0dd198b75fd8c14965
ches613 <zverkovmi@gmail.com>
2023-01-10 20:38:25 +0400

    add task of homework 10

---
+++ homework_10/views.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update, ReplyKeyboardMarkup, ParseMode, \
+    ReplyKeyboardRemove, Dice
+from telegram.ext import CallbackContext, ConversationHandler
+
+
